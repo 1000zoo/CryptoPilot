12187d01a32e4ef801b0880a53927714e0f0b72b
1000zoo <cjswldn99@gmail.com>
2024-02-28 20:46:44 +0900

    feat: predict 메서드 완성 상승시 1, 하락시 -1, 그 외 0 횡보로 예상 기준은  THRESHOLD 상수를 조절하여 변경 가능

--- src/test/cryptoAI/predicator_test.py
+++ src/test/cryptoAI/predicator_test.py
@@ -2,6 +2,7 @@ import unittest
 import pyupbit
 import os
 import sys
+import datetime
 
 root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../'))
 sys.path.append(root_path)
@@ -10,18 +11,29 @@ from src.main.cryptoAI.preprocess import *
 from src.main.cryptoAI.predicator import *
 
 
-
 class PredicatorTest(unittest.TestCase):
-    pass
+    def test_encode(self):
+        from src.main.cryptoAI.predicator import _encode, TRADING_THRESHOLD
+        self.assertEqual(_encode(np.array([0.3, 0.3 + TRADING_THRESHOLD * 1.1])), 1)
+        self.assertEqual(_encode(np.array([0.3, 0.3 + TRADING_THRESHOLD / 10])), 0)
+        self.assertEqual(_encode(np.array([0.3, 0.3 - TRADING_THRESHOLD * 1.1])), -1)
+
 
 
 def test_model_predict():
     data = preprocessing(pyupbit.get_ohlcv("KRW-BTC", "minute1", 6))
     result = model_predict(data)
-    import datetime
     print(datetime.datetime.now())
     print(result)
 
+def test_predict():
+    print(datetime.datetime.now())
+    p = predict()
+    print(p)
+
+
 if __name__ == "__main__":
-    test_model_predict()
+    # test_model_predict()
+    test_predict()
+    # unittest.main()
 
