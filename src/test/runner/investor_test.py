3e17b5ce02b014616fa5111d1ceae57382dcc024
1000zoo <cjswldn99@gmail.com>
2024-03-01 05:12:30 +0900

    refactor: Invastor 오타 수정 => Investor

--- src/test/runner/investor_test.py
+++ src/test/runner/investor_test.py
@@ -6,7 +6,7 @@ import datetime
 root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../'))
 sys.path.append(root_path)
 
-from src.main.runner.invastor import *
+from src.main.runner.investor import *
 from src.test.mockUpbit.mockUpbit import *
 
 
@@ -14,9 +14,9 @@ def test_log():
     logger = Logger("tes1t_na311me")
     logger.write_trade_log("BTC", "buy", 44.44, 55.55, 66.66, 77.77)
 
-class InvastorTest:
+class InvestorTest:
     def __init__(self) -> None:
-        self.user = Investor(MockUpbit(100000, 0), Logger("invastorTest"))
+        self.user = Investor(MockUpbit(100000, 0), Logger("investorTest"))
     
     def test_buy(self):
         self.user.buy_market_order("KRW-BTC", 10000)
@@ -24,5 +24,5 @@ class InvastorTest:
 if __name__ == "__main__":
     # test_log()
 
-    invastor = InvastorTest()
-    invastor.test_buy()
+    investor = InvestorTest()
+    investor.test_buy()
