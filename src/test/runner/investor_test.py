4e80192af29a33952afa6d41a31cbe1934aeda48
1000zoo <cjswldn99@gmail.com>
2024-03-05 17:09:30 +0900

    feat: 변경

--- src/test/runner/investor_test.py
+++ src/test/runner/investor_test.py
@@ -1,6 +1,7 @@
 import pyupbit as pu
 import os
 import sys
+import time
 import datetime
 
 root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../'))
@@ -21,8 +22,52 @@ class InvestorTest:
     def test_buy(self):
         self.user.buy_market_order("KRW-BTC", 10000)
 
+
+def cal(investor : Investor):
+    state = predict()
+    print(state)
+    btc_balance = investor.get_balance("BTC")
+    krw_balance = investor.get_balance("KRW")
+    
+    if btc_balance > 0:
+        if state==0 or state==1:
+            pass
+        else:
+            investor.sell_market_order("KRW-BTC", btc_balance)
+    else:
+        if state==1:
+            investor.buy_market_order("KRW-BTC", krw_balance)
+        else:
+            pass
+
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d - %H:%M:%S")
+    krw = investor.get_balance("KRW")
+    btc = investor.get_balance("BTC")
+    btc_amount = investor.get_amount("BTC")
+    print(f"{current_time} ==> krw: {krw}, btc: {btc}, btc to krw: {btc_amount}")
+
+def loop_test():
+    previous_minute = None
+    cnt = 0
+    investor = Investor(MockUpbit(10000000, 0), 
+                        Logger(str(datetime.datetime.now().strftime("%Y-%m-%d - %H-%M"))))
+    while cnt<60 * 300:
+        current_time = datetime.datetime.now()
+        current_minute = current_time.minute
+        #시간이 변경됐으면 매매함수 호출
+        if previous_minute==None or current_minute!=previous_minute:
+            previous_minute=current_minute
+            cal(investor)
+        #1초마다 시간 확인
+        time.sleep(1)
+        cnt+=1
+
+def main():
+    loop_test()
+
 if __name__ == "__main__":
     # test_log()
 
-    investor = InvestorTest()
-    investor.test_buy()
+    # investor = InvestorTest()
+    # investor.test_buy()
+    main()
