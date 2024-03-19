4de6048ed2b83da4797a4eccffdc87683a89ec3d
sungyeong98 <am25227903@gmail.com>
2024-03-19 17:02:07 +0900

    feat: trade_engine 기능 구현

--- src/main/tradeEngine/trade_engine.py
+++ src/main/tradeEngine/trade_engine.py
@@ -4,55 +4,53 @@ import time
 from src.main.cryptoAI.predicator import predict
 from src.main.runner.investor import Investor
 from src.main.tradeEngine.ema_equation import *
+from src.main.tradeEngine.rsi_equation import rsi
 
 #매수, 매도 진행 코드
 def trade_engine(investor : Investor):
-    #현재 비트코인의 가격(보유중인 가격 혹은 현재 시장가격)
-    #현재 가격에 대한 코드는 추가 작업이 필요함(보유중이지 않다면 예측값을 저장해서 가지고 있어야함)
-    #current_price=upbit.get_balance('KRW-BTC')['avg_buy_price'] if flag else 0
-    krw_amount = investor.get_balance("KRW")
-    btc_amount = investor.get_amount("KRW")
-    flag = btc_amount > 0
-
-    #1분뒤 예측되는 비트코인의 가격
-    #predict_price=예측값 호출
-    predict_state = predict()
-    dis_state=discriminant()
-    #현재 비트코인을 보유하고 있을 때
-    if flag:
-        #예측값이 더 오를것으로 예상될 경우, 아무 행동도 취하지 않음
-        if predict_state == 1 and dis_state:
-            pass
-        #예측값이 떨어질 것으로 예상될 경우, 매도 진행
+    prev_state=None
+    cnt=0
+    while cnt<=4:
+        state = rsi()
+        if state==0:
+            return 0
+        if prev_state==None:
+            prev_state=state
+            continue
+        
+        if prev_state==state:
+            cnt+=1
         else:
-            investor.sell_market_order("KRW-BTC", btc_amount)       #수량은 나중에 따로 관리
-    #비트코인을 보유하고 있지 않을 때
-    else:
-        #예측값이 오를 것으로 예상될 경우, 매수 진행
-        if predict_state ==1 and dis_state:
-            investor.buy_market_order("KRW-BTC", krw_amount)     #수수료 제외
-        #예측값이 떨어질 것으로 예상될 경우, 아무 행동도 취하지 않음
-        else:
-            pass
+            return 0
+        time.sleep(1)
 
+    return state   
 
 def main(investor : Investor):
     #이전 시간 기록
     previous_minute=None
-
+    
     #매 분마다 매매 진행
     while True:
-
         current_time=datetime.datetime.now()
         current_minute=current_time.minute
 
-        #시간이 변경됐으면 매매함수 호출
+        state=trade_engine(investor)
+
+        flag=investor.get_balance("BTC")>0
+        
+        if flag:
+            if state==-1:
+                investor.sell_market_order('KRW-BTC',investor.get_balance('BTC'))
+        else:
+            if state==1:
+                investor.buy_market_order("KRW-BTC",investor.get_balance('KRW'))
+
         if previous_minute==None:
             previous_minute=current_minute
-
+        
         elif current_minute!=previous_minute:
             previous_minute=current_minute
-            trade_engine(investor)
             print(current_time,investor.get_balance("KRW"),investor.get_balance("BTC"))
         #1초마다 시간 확인
         time.sleep(1)
