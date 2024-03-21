04bb3037c0a1e85672485bbc4463a1ee8d7b1f1d
sungyeong98 <am25227903@gmail.com>
2024-03-21 16:00:30 +0900

    feat: rsi_equation 수정

--- src/main/tradeEngine/rsi_equation.py
+++ src/main/tradeEngine/rsi_equation.py
@@ -6,17 +6,17 @@ import pandas_ta as ta
 def rsi():
     data=ta.rsi(pu.get_ohlcv("KRW-BTC",interval='minute1',count=200)['close'],length=14)
 
-    check_num=data.iloc[-1]
+    check_num=data.iloc[-2]
     check_pos=data.iloc[-1]-data.iloc[-2]
 
     # 0 : stop, 1 : buy, -1 : sell
-    if check_num>=65 and check_pos<0:
+    if check_num>=70 and check_pos<0:
         return -1
-    elif check_num>=65 and check_pos>0:
+    elif check_num>=70 and check_pos>0:
         return 0
-    elif check_num<=35 and check_pos>0:
+    elif check_num<=30 and check_pos>0:
         return 1
-    elif check_num<=35 and check_pos<0:
+    elif check_num<=30 and check_pos<0:
         return 0
     else:
         return 0
